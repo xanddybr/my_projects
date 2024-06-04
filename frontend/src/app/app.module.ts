f7d204c825a40471461b5bd180c47418032c75b6
xanddybr <xanddybr@gmail.com>
2024-06-04 11:16:28 -0300

    fixed git

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,7 +13,9 @@ import {MatSidenavModule} from '@angular/material/sidenav'
 import {MatCardModule} from '@angular/material/card'
 import {MatListModule} from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive'
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     FooterComponent,
     NavComponent,
     HomeComponent,
-    ProductCrudComponent
+    ProductCrudComponent,
+    RedDirective,
+    ForDirective
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit {
+
+  @Input('myForEm') numbers: number[]
+
+  constructor() {
+   
+  }
+
+  ngOnInit(): void {
+    console.log(this.numbers) 
+  }
+    
+}
