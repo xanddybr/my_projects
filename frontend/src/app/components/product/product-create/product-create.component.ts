be8e0c87b9c524c4002a33cde6d2b28c78cdbbbb
xanddybr <xanddybr@gmail.com>
2024-06-04 17:56:49 -0300

    0406.1756

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.css']
 })
-export class ProductCreateComponent {
+export class ProductCreateComponent implements OnInit {
+
+atributoLegal = 'qualquer'
+
+constructor(){ }
+
+propLegal = "Propiedade da binding"
+
+ngOnInit(): void {
+
+}
+
+fazerAlgo(): void {
+
+
+}
 
 }
